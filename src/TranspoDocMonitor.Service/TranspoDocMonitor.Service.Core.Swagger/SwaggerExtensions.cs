9c39e460d42b4324e75cec7ef5a277d7e17707f6
Barracuda <bredinin1@gmail.com>
2024-04-12 22:12:31 +0300

    11

---
+++ src/TranspoDocMonitor.Service/TranspoDocMonitor.Service.Core.Swagger/SwaggerExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace TranspoDocMonitor.Service.Core.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static IApplicationBuilder UseSwaggerCustom(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            return app;
+        }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "bearer",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "Bearer"}
+                        },
+                        new List<string>()
+                    }
+                });
+
+                options.OperationFilter<SwaggerHeader>();
+
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1.0",
+                    Title = "ELDabaa",
+                    Description = "Backend API",
+                });
+
+            });
+
+            return services;
+        }
+    }
+}
